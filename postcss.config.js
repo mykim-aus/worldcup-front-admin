3e5957bcb303a1debff32da025fd327186ad7901
Juhyung Hong <ainu0516@gmail.com>
2023-06-10 16:31:18 +0900

    main branch default setting

---
+++ postcss.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  contents: [
+    "./app/**/*.{js,jsx,ts,tsx}",
+    "./pages/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+  ],
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};

47eed70c406975bb25b6f0c1bf4dc95f04e11ce7
Juhyung Hong <ainu0516@gmail.com>
2023-07-22 16:50:53 +0900

    i18n 수정

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,10 +1,14 @@
 import { useTranslations } from 'next-intl';
 
 export default function Home() {
-  const t = useTranslations('common2');
+  const t = useTranslations('common');
+  const r = useTranslations('auth');
+  const s = useTranslations();
   return (
     <div className="text-green-600">
       <p>{t('edit')}</p>
+      <p>{r('login')}</p>
+      <p>{s('common.regist')}</p>
     </div>
   );
 }
--- i18n.ts
+++ i18n.ts
@@ -1,5 +1,24 @@
 import { getRequestConfig } from 'next-intl/server';
+import fs from 'fs';
+import path from 'path';
 
-export default getRequestConfig(async ({ locale }) => ({
-  messages: (await import(`./messages/${locale}/common.json`)).default,
-}));
+const getTranslations = async (locale: string) => {
+  const messagesDir = path.resolve(`./messages/${locale}`);
+  const filenames = fs.readdirSync(messagesDir);
+  const messages: any = {};
+
+  for (const filename of filenames) {
+    const namespace = path.basename(filename, '.json');
+    const filePath = path.join(messagesDir, filename);
+    const fileContent = fs.readFileSync(filePath, 'utf8');
+
+    messages[namespace] = JSON.parse(fileContent);
+  }
+
+  return messages;
+};
+
+export default getRequestConfig(async ({ locale }) => {
+  const messages = await getTranslations(locale);
+  return { messages };
+});

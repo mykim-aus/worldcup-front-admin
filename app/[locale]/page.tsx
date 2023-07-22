5385d7b362389913f4898c8d758a95c59b9f1e2a
Juhyung Hong <ainu0516@gmail.com>
2023-07-22 16:18:54 +0900

    구조 수정 1차

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,11 +1,10 @@
 import { useTranslations } from 'next-intl';
 
 export default function Home() {
-  const t = useTranslations('home');
-
+  const t = useTranslations('common2');
   return (
     <div className="text-green-600">
-      <p>{t('title')}</p>
+      <p>{t('edit')}</p>
     </div>
   );
 }
